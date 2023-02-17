222d13a15a27d4653dd0c24b942eb2cc6e74d221
Liyourong <wangliyourong@gmail.com>
2023-02-17 10:26:03 -0500

    Initialize cpp file

--- scripts/day_1.cpp
+++ scripts/day_1.cpp
@@ -1,2 +1,28 @@
-// this is a day one file 
+#include <iostream>
+#include <set>
 
+using namespace std;
+
+// this is a day one file
+
+int main()
+{
+
+    set<int> s;
+    s.insert(1);
+    s.insert(6);
+    s.insert(3);
+     s.insert(1);
+
+    //
+
+    cout<<"sdad"<<endl;
+
+    for (auto it : s)
+    {
+
+        cout << it << endl;
+    }
+
+    return 0;
+}
